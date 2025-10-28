01fb6fb9041b361661a25714c39bd6fb49069b4c
grabbedbyalienspayments-eng <grabbedbyalienspayments@gmail.com>
2025-10-28 04:03:20 +0200

    Refactor Vite config by removing unused variables
    Removed base path and preview environment variables from config.

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 import { resolve } from 'node:path'
 import AutoImport from 'unplugin-auto-import/vite'
 
-const base = process.env.BASE_PATH || '/'
-const isPreview = process.env.IS_PREVIEW ? true : false;
-
-// https://vite.dev/config/
 export default defineConfig({
-  define: {
-   __BASE_PATH__: JSON.stringify(base),
-   __IS_PREVIEW__: JSON.stringify(isPreview)
-  },
-  plugins: [react(),
+  plugins: [
+    react(),
     AutoImport({
       imports: [
         {
@@ -56,7 +49,6 @@ export default defineConfig({
             'Outlet'
           ]
         },
-        // React i18n
         {
           'react-i18next': [
             'useTranslation',
@@ -67,9 +59,9 @@ export default defineConfig({
       dts: true,
     }),
   ],
-  base,
+  // ✅ No base override here
   build: {
-    sourcemap: true,
+    sourcemap: false,
     outDir: 'dist',
   },
   resolve: {
